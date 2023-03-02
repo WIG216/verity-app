f271ad6a5c1d2c92fca52293c5f72687f9fd4b06
EnthCliff <89383770+WIG216@users.noreply.github.com>
2023-03-03 00:45:49 +0100

    done sessions controller

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
-    //
+    // show login form
+    public function login(Request $request)
+    {
+        return view('session.login-session');
+    }
+
+    // authentication user
+    public function authentication(Request $request)
+    {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+
+        if(auth()->attempt($formFields, 'remember')){
+            $request -> session()->regenerate();
+
+            return redirect('dashboard')->with(['success'=>'You are logged in.']);
+        }
+
+        return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
+    }
+
+     // logout user
+     public function logout(Request $request)
+     {
+         auth()->logout();
+
+         $request->session()->invalidate();
+         $request->session()->regenerateToken();
+
+         return redirect('/login')->with('message', 'You have been logged out');
+     }
 }
