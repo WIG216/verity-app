321b7f957c1942c5500b2e9e4a7e92a1cad20c0e
EnthCliff <89383770+WIG216@users.noreply.github.com>
2023-03-03 01:19:17 +0100

    done login

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,7 +16,7 @@ class SessionsController extends Controller
     public function authentication(Request $request)
     {
         $formFields = $request->validate([
-            'email' => ['required', 'email'],
+            'name' => ['required', 'name'],
             'password' => 'required'
         ]);
 
--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" type="text/css" href="assets/css/forms/switches.css">
 </head>
 <body class="form">
-    
+
 
     <div class="form-container outer">
         <div class="form-form">
@@ -26,14 +26,18 @@
 
                         <h1 class="">Sign In</h1>
                         <p class="">Log in to your account to continue.</p>
-                        
-                        <form class="text-left">
+
+                        <form class="text-left" method="POST" action="/session">
+                            @csrf
                             <div class="form">
 
                                 <div id="username-field" class="field-wrapper input">
                                     <label for="username">USERNAME</label>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
-                                    <input id="username" name="username" type="text" class="form-control" placeholder="e.g John_Doe">
+                                    <input id="username" name="name" type="text" class="form-control" placeholder="Username">
+                                    @error('name')
+                                    <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                  @enderror
                                 </div>
 
                                 <div id="password-field" class="field-wrapper input mb-2">
@@ -43,6 +47,9 @@
                                     </div>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>
                                     <input id="password" name="password" type="password" class="form-control" placeholder="Password">
+                                    @error('password')
+                                    <p class="text-danger text-xs mt-2">{{ $message }}</p>
+                                  @enderror
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="toggle-password" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                                 </div>
                                 <div class="d-sm-flex justify-content-between">
@@ -57,7 +64,7 @@
 
                                 <div class="social">
                                     <a href="javascript:void(0);" class="btn social-fb">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg> 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
                                         <span class="brand-name">Facebook</span>
                                     </a>
                                    <a href="javascript:void(0);" class="btn social-github">
@@ -71,18 +78,18 @@
                             </div>
                         </form>
 
-                    </div>                    
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    
+
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
     <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
     <script src="bootstrap/js/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
-    
+
     <!-- END GLOBAL MANDATORY SCRIPTS -->
     <script src="assets/js/authentication/form-2.js"></script>
 
